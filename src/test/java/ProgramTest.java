6d4eb7a9248cf922644aa9bc3663915f2b4737c4
MarkusNyland <nylmar15@student.westerdals.no>
2018-09-03 11:11:19 +0200

    Added failing test

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,11 +1,17 @@
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ProgramTest {
 
     @Test
-    public void test(){
+    public void testPass(){
         assertTrue(true);
+    }
+
+    @Test
+    public void testFail(){
+        fail();
     }
 
 }
