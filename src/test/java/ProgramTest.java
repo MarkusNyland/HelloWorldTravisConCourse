47b563878f12a6a22d9394c1e9f4045333c50e99
MarkusNyland <nylmar15@student.westerdals.no>
2018-09-03 11:37:08 +0200

    Revert "Added failing test"
    This reverts commit 6d4eb7a9248cf922644aa9bc3663915f2b4737c4.

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,17 +1,11 @@
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class ProgramTest {
 
     @Test
-    public void testPass(){
+    public void test(){
         assertTrue(true);
-    }
-
-    @Test
-    public void testFail(){
-        fail();
     }
 
 }
